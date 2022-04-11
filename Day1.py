c58f0d08f45cb89241a820f3c89d7dcfb8fa2fa4
justanadmin <103416575+justanadmin@users.noreply.github.com>
2022-04-11 22:18:06 +0530

    Create Day1.py
    About Strings and storing values as variable. Swapping is also mentioned. Print name without storing

---
+++ Day1.py
@@ -0,0 +1,51 @@
+from unicodedata import name
+
+
+print("Namaste World!")
+print("Namaste World! \n Hello World")
+print("Hello" + " " + "Angela") # adding strings 
+NameOftheCandidate = input("What is your name")
+print ("Go to Hell" + " " +input("What is your name")+"!")#print without saving data in a variable
+
+# Length of your name
+
+Name = input("Enter your name")
+lengthOfName = len(Name)
+print(lengthOfName)
+
+#length of your name without storing in variable 
+
+print ( len (input ("Enter your name ?")))
+
+## Switching Values Question Provided by instructor
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c=b
+b=a
+a=c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+# 🚨 variables in python
+
+user_name = input("Enter User Name:")
+print(user_name)
+
+# 🚨 Generate the band name project 
+
+City_name = input("Enter your city name\n")
+Pet_name = input("Enter your pet name\n")
+print("The Brand Name is "+City_name+" "+Pet_name)
