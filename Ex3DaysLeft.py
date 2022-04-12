42ff9fcd1071064fc1aa39b03bc2d5deb6143449
justanadmin <103416575+justanadmin@users.noreply.github.com>
2022-04-13 01:05:11 +0530

    Create Ex3DaysLeft.py
    Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
    
    It will take your current age as the input and output a message with our time left in this format:
    
    You have x days, y weeks, and z months left.
    
    Where x, y and z are replaced with the actual calculated numbers.
    
    Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.

---
+++ Ex3DaysLeft.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+leftOverAge=90-int(age)
+x=leftOverAge*365
+y=leftOverAge*52
+z=leftOverAge*12
+print(f"You have {x} days, {y} weeks, and {z} months left.")
+
+
